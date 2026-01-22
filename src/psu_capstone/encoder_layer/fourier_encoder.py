5acb59ad0e6a4e4405fec0e17e1a0090efdf6167
millscb <christopher.b.mills@gmail.com>
2026-01-21 18:22:31 -0600

    make update

--- src/psu_capstone/encoder_layer/fourier_encoder.py
+++ src/psu_capstone/encoder_layer/fourier_encoder.py
@@ -1,8 +1,10 @@
+from typing import override
+
 import numpy as np
-from scipy.ftt import fft, ifft
+from scipy.fft import fft, ifft
 
 from psu_capstone.encoder_layer.base_encoder import BaseEncoder
-from psu_capstone.encoder_layer.rdse import RandomSparseRepresentation
+from psu_capstone.encoder_layer.rdse import RandomDistributedScalarEncoder
 from psu_capstone.sdr_layer.sdr import SDR
 
 
@@ -13,17 +15,14 @@ class FourierEncoder(BaseEncoder):
         super().__init__(input_dim, output_dim)
         if output_dim % 2 != 0:
             raise ValueError("Output dimension must be even for Fourier encoding.")
-        self.half_output_dim = output_dim // 2
 
-    def encode(self, x, output_sdr: SDR):
+        self.half_output_dim = output_dim // 2
+        self._input_dim = input_dim
+        self._output_dim = output_dim
+
+        self._rdse = RandomDistributedScalarEncoder()
+
+    @override
+    def encode(self, x, output_sdr: SDR) -> None:
         # Perform FFT on the input data
         fft_result = fft(x, n=self.half_output_dim, axis=-1)
-
-        # Separate real and imaginary parts
-        real_part = np.real(fft_result)
-        imag_part = np.imag(fft_result)
-
-        # Concatenate real and imaginary parts
-        encoded = np.concatenate([real_part, imag_part], axis=-1)
-
-        return encoded
