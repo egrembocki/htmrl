2df53b7cfc5660ee6695893ee017b6f3bd57e3a2
millscb <christopher.b.mills@gmail.com>
2026-01-29 23:32:40 -0600

    changed max scalar encode back to 1000

--- src/psu_capstone/encoder_layer/scalar_encoder.py
+++ src/psu_capstone/encoder_layer/scalar_encoder.py
@@ -29,7 +29,7 @@ class ScalarEncoderParameters:
      * Members "minimum" and "maximum" define the range of the input signal.
      * These endpoints are inclusive.
      */"""
-    maximum: int = 5000
+    maximum: int = 1000
     """Min and Max
      * Members "minimum" and "maximum" define the range of the input signal.
      * These endpoints are inclusive.
