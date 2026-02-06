db2f38a5d616cd4ad381e4e15a1caeda3d2ab8ad
millscb <christopher.b.mills@gmail.com>
2026-02-06 15:24:47 -0600

    linting

--- src/psu_capstone/__init__.py
+++ src/psu_capstone/__init__.py
@@ -1,4 +1,14 @@
-from psu_capstone import agent_layer, encoder_layer, utils
+import os
+from importlib.metadata import version
+
+from src.psu_capstone import agent_layer, encoder_layer, utils
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(PROJECT_ROOT, "data")
+
+
+__version__ = version("psu_capstone")
+
 
 __all__ = [
     "encoder_layer",
