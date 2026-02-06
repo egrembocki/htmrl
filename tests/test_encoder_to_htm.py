e9a07044bc9216a1e7a026dd1428c97330b755a0
millscb <christopher.b.mills@gmail.com>
2026-02-05 22:13:36 -0600

    linting

--- tests/test_encoder_to_htm.py
+++ tests/test_encoder_to_htm.py
@@ -1,9 +1,7 @@
-import pandas as pd
 import pytest
 
 from psu_capstone.encoder_layer.base_encoder import BaseEncoder
 from psu_capstone.input_layer.input_handler import InputHandler
-from psu_capstone.sdr_layer.sdr import SDR
 
 
 class DummyEncoder(BaseEncoder):
@@ -12,10 +10,10 @@ class DummyEncoder(BaseEncoder):
         if dimensions is None:
             dimensions = [8, 1]  # arbitrary SDR size for the test
 
-    def attach_input(self, df: pd.DataFrame):
-        self.input_df = df
+    def attach_input(self, records: list[dict[str, float]]):
+        self.input_records = records
 
-    def encode(self, input_value: float, output_sdr: SDR) -> None:
+    def encode(self, input_value: float) -> None:
         pass
 
 
@@ -30,10 +28,10 @@ class HTMinterface:
 
     """
 
-    def __init__(self, sdr: SDR):
+    def __init__(self, sdr: list[int]):
         self.last_received_sdr = sdr
 
-    def consume_sdr(self, sdr: SDR):
+    def consume_sdr(self, sdr: list[int]):
         self.last_received_sdr = sdr
 
 
@@ -42,28 +40,26 @@ def test_encoder_to_htm_receives_sdr_object():
     # Arrange
     fib_sequence = [0, 1, 1, 2, 3, 5, 8, 13]
 
-    handler = InputHandler()
-    df = handler._to_dataframe(fib_sequence)
-
     encoder = DummyEncoder()
-    encoder.attach_input(df)
+    handler = InputHandler()
+    records = handler.input_data(fib_sequence, required_columns=["value"])
+    buffered_records = encoder.buffer_data(records)
+    encoder.attach_input(buffered_records)
 
     # HTM interface (not implemented yet)
-    htm = HTMinterface(sdr=SDR([8, 1]))
+    htm = HTMinterface([8, 1])
 
     # Act
     # Encode a single value
-    last_value = df.iloc[3].values[0]
-    sdr = SDR([8, 1])
-    encoder.encode(last_value, sdr)
+    last_value = buffered_records[3]["value"]
+    # sdr = SDR([8, 1])
+    e = encoder.encode(last_value)
 
     # Mock interface call: Would accept an SDR instance as input.
-    htm.consume_sdr(sdr)
+    htm.consume_sdr(e)
 
     # Assert
     # Once HTMinterface is implemented, give it some observable state
-    assert isinstance(df, pd.DataFrame)
+    assert isinstance(buffered_records, list)
     assert last_value == 2
-    assert df.shape == (8, 1)
-    assert isinstance(sdr, SDR)
-    assert htm.last_received_sdr is sdr
+    assert len(buffered_records) == 8
