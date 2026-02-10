b3f9f98345371188fe534d2af9dded0dbb4b5678
millscb <christopher.b.mills@gmail.com>
2026-02-10 11:04:32 -0600

    u

--- src/psu_capstone/__init__.py
+++ src/psu_capstone/__init__.py
@@ -1,8 +1,1 @@
-import os
-from importlib.metadata import version
-
-__version__ = version("psu_capstone")
-
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_PATH = os.path.join(PROJECT_ROOT, "data", "easyData.xlsx")
+__all__ = 
