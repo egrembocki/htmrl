9eb55941a3cfdd9f1ba83ad03a9d5c0148d6d831
millscb <christopher.b.mills@gmail.com>
2026-03-06 17:23:50 -0600

    linting

--- src/psu_capstone/encoder_layer/delta_encoder.py
+++ src/psu_capstone/encoder_layer/delta_encoder.py
@@ -10,7 +10,7 @@ from psu_capstone.encoder_layer.base_encoder import BaseEncoder, ParentDataClass
 from psu_capstone.encoder_layer.rdse import RandomDistributedScalarEncoder, RDSEParameters
 
 
-class DeltaEncoder(BaseEncoder[float]):
+class DeltaEncoder(BaseEncoder[tuple[float, float]]):
     """Encodes the difference between two values."""
 
     def __init__(self, encoder_params: DeltaEncoderParameters | None = None):
@@ -24,14 +24,33 @@ class DeltaEncoder(BaseEncoder[float]):
         self._size = params.size
         self._sparsity = params.sparsity
         self._active_bits = params.active_bits
-        self._cached_encodings = {}
+        self._cached_encodings: dict[float, list[int]] = {}
+        self._delta_value = 0.0
+
+        self._rdse_encoder = RandomDistributedScalarEncoder(
+            RDSEParameters(size=params.size, sparsity=params.sparsity)
+        )
 
         super().__init__(params.size)
 
     @override
-    def encode(self, input_value: float) -> list[int]:
+    def encode(self, input_value: tuple[float, float]) -> list[int]:
         """Encode the difference between value1 and value2."""
-        raise NotImplementedError("DeltaEncoder does not implement encode()")
+
+        value1, value2 = input_value
+        if value1 > value2:
+            self._delta_value = value1 - value2
+        else:
+            self._delta_value = value2 - value1
+
+        # return early if we have already computed the encoding for this delta value
+        if self._delta_value in self._cached_encodings:
+            return self._cached_encodings[self._delta_value]
+
+        encoding = self._rdse_encoder.encode(self._delta_value)
+        self._cached_encodings[self._delta_value] = encoding
+
+        return encoding
 
     def decode(self, sdr: list[int]) -> tuple[float, float]:
         """Decode the SDR back to a value and confidence."""
@@ -53,3 +72,10 @@ class DeltaEncoderParameters(ParentDataClass):
 
     active_bits: int = 0
     """The number of bits that are active in the output SDR."""
+
+
+if __name__ == "__main__":
+    encoder = DeltaEncoder()
+    input_value = (10.0, 5.0)
+    encoding = encoder.encode(input_value)
+    print(f"Input: {input_value}, Encoding: {encoding}")
