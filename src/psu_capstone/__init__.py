95ca3a1c5b9a7e2cf85afe3d736ba92f295fa682
millscb <christopher.b.mills@gmail.com>
2026-03-17 17:25:35 -0500

    pdocs

--- src/psu_capstone/__init__.py
+++ src/psu_capstone/__init__.py
@@ -10,11 +10,6 @@ Attributes:
 import os
 from importlib.metadata import PackageNotFoundError, version
 
-from .encoder_layer.base_encoder import BaseEncoder, ParameterMarker
-from .encoder_layer.category_encoder import CategoryEncoder, CategoryParameters
-from .encoder_layer.rdse import RandomDistributedScalarEncoder, RDSEParameters
-from .encoder_layer.scalar_encoder import ScalarEncoder, ScalarEncoderParameters
-
 try:
     __version__ = version("psu_capstone")
 except PackageNotFoundError:
@@ -23,14 +18,3 @@ except PackageNotFoundError:
 PYTHONPATH = "/src"
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "easyData.xlsx")
-
-__all__ = [
-    "BaseEncoder",
-    "ParameterMarker",
-    "CategoryEncoder",
-    "CategoryParameters",
-    "RDSEParameters",
-    "RandomDistributedScalarEncoder",
-    "ScalarEncoder",
-    "ScalarEncoderParameters",
-]
