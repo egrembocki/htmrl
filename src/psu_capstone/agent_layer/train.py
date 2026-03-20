189b9678cee3df7329d81ce4638d524b12be3f8e
millscb <christopher.b.mills@gmail.com>
2026-03-20 13:29:17 -0500

    test

--- src/psu_capstone/agent_layer/train.py
+++ src/psu_capstone/agent_layer/train.py
@@ -178,7 +178,7 @@ class Trainer:
         """Setup the ColumnField for the Brain."""
         column_field = ColumnField(
             input_fields=self._trainer_input_fields,
-            non_spatial=True,
+            non_spatial=False,
             num_columns=num_columns,
             cells_per_column=cells_per_column,
         )
